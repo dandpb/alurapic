fef5a1d5874fa14861c4c62f63ae28b950bf68a5
= <flaviohenriquealmeida@gmail.com>
2016-11-09 16:39:53 -0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
+import { NgModule }  from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { PainelModule } from './painel/painel.module';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { routing }  from './app.routes';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BotaoModule } from './botao/botao.module';
+import { ModalModule } from './modal/modal.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [ 
+        BrowserModule, 
+        FotoModule, 
+        HttpModule, 
+        PainelModule, 
+        routing, 
+        FormsModule, 
+        ReactiveFormsModule,
+        BotaoModule, 
+        ModalModule
+    ],
+    declarations: [ AppComponent, CadastroComponent, ListagemComponent ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
